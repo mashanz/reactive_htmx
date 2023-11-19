9b62796ec2da1ecc7747290b09e70f1db811205f
Mashanz <mangatkk@gmail.com>
2023-11-19 12:58:07 +0700

    delete

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("components/", views.components, name="components"),
     path("contents/", views.contents, name="contents"),
-    path("reactive/", views.reactive, name="reactive"),
+    path("reactive/", views.reactive_get, name="reactive_get"),
+    path("reactive/add/", views.reactive_add, name="reactive_add"),
+    path("reactive/delete/", views.reactive_delete, name="reactive_delete"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.template import engines
 from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
-from django.middleware.csrf import get_token
 
 # make it global because I am lazy to install/config redis here, well harus nya ini di masukin ke redis LOL 🤣
 object_lists = []
@@ -24,49 +22,49 @@ def contents(request):
     context ={'MENU': q}
     return render(request, f"home/contents/{q}.html", context)
 
-# Reactive Forma LOL 🤣
-@require_http_methods(["GET", "POST", "PUT", "DELETE"])
-def reactive(request):
-    #object_lists = []
-
-    if request.method == 'POST':
-        object_lists.append({
-            "id": 0,
-            "type": "text",
-            "label": "label",
-            "placeholder": "Placeholder Text",
-            "helper_text": "Helper Text",
-        })
-    elif request.method == 'DELETE':
-        object_lists.pop(request.GET.get("id"))
-
+def render_html(obj):
     str_template = ""
-    csrf_token = get_token(request)
-    csrf_token_html = '<input type="hidden" name="csrfmiddlewaretoken" value="{}" />'.format(csrf_token)
-    for ol in object_lists:
+    for ol in obj:
         str_template += f"""
-        <div>
-            <div>
+        <div class="card bg-base-100 p-4">
+            <div class="p-2">
                 <label for='form_{ol['id']}'><b>{ol['label']}</b></label>
             </div>
             <input 
+                class="input outline"
                 type='{ol['type']}' 
                 id='form_{ol['id']}' 
                 name='form_{ol['id']}' 
                 placeholder='{ol['placeholder']}'>
-            <div>
+            <div class="p-2">
                 <i>{ol['helper_text']}</i>
             </div>
-            <form hx-delete="/reactive/?id={ol['id']}" hx-triger="submit" hx-target="#object_lists">
-                {csrf_token_html}
-                <button class="btn btn-danger" type="submit">
-                    DELETE
-                </buton>
-            </form>
+            <button class="btn btn-error" hx-get="/reactive/delete/?id={ol['id']}" hx-target="#object_lists">
+                DELETE
+            </buton>
         </div>
         """
 
-    print(object_lists)
     django_engine = engines["django"]
     template = django_engine.from_string(str_template)
     return HttpResponse(template.render())
+
+# Delete ADD
+def reactive_add(request):
+    object_lists.append({
+        "id": 0,
+        "type": "text",
+        "label": "label",
+        "placeholder": "Placeholder Text",
+        "helper_text": "Helper Text",
+    })
+    return render_html(object_lists)
+
+# Delete
+def reactive_delete(request):
+    object_lists.pop(int(request.GET.get("id")))
+    return render_html(object_lists)
+
+# Reactive Form LOL 🤣
+def reactive_get(request):
+    return render_html(object_lists)
