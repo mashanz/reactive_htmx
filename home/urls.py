2398721dd024a362914c20c6480bf01d77431fbe
Mashanz <mangatkk@gmail.com>
2023-11-19 10:22:41 +0700

    reactive pages navigations

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("components/", views.components, name="components"),
+    path("contents/", views.contents, name="contents"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+# Create your views here. well, ini cuma index 🧪
 def index(request):
-    return render(request, "home/index.html")
+    return render(request, "home/index.html", {'MENU': ""})
+
+
+# SSR Component LOL 🤣
+def components(request):
+    q = request.GET.get("q")
+    return render(request, f"home/components/{q}.html")
+
+
+# SSR Content LOL 🤣
+def contents(request):
+    q = request.GET.get("q")
+    print(q)
+    context ={'MENU': q}
+    return render(request, f"home/contents/{q}.html", context)
